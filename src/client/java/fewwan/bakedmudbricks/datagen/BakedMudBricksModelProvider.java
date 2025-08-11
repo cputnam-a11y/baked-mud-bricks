6145934f35aae09f2449ee347dc1c2d9b664aaa0
cputnam-a11y <unknown@cputnam-a11y.github.io>
2025-08-11 01:07:38 -0500

    Update to 1.21.8 Adopt Datagen over manual resources

--- src/client/java/fewwan/bakedmudbricks/datagen/BakedMudBricksModelProvider.java
+++ src/client/java/fewwan/bakedmudbricks/datagen/BakedMudBricksModelProvider.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.block.Block;
 import net.minecraft.client.data.*;
-import net.minecraft.client.render.model.json.WeightedVariant;
 import net.minecraft.util.Identifier;
 
 public class BakedMudBricksModelProvider extends FabricModelProvider {
